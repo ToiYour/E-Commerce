2c121470caa4e4aeac29c28c6f38d211c4583550
ToiYour <toidz25102004@gmail.com>
2024-08-28 09:17:50 +0700

    Close Message Limit API Render

--- FE/src/layouts/LayoutHome.tsx
+++ FE/src/layouts/LayoutHome.tsx
@@ -2,15 +2,8 @@ import ChatClient from "@/pages/users/Chat";
 import Footer from "@/pages/users/Footer";
 import Header from "@/pages/users/Headers";
 import { Outlet, ScrollRestoration } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-
+import { ToastContainer } from "react-toastify";
 const LayoutHome = () => {
-  toast.error(
-    "Backend mình sử dụng render miễn phí đang bị giới hạn, bạn có thể truy cập vào lúc khác nhé. Cảm ơn bạn đã ghé thăm!"
-  );
-  console.error(
-    "Backend mình sử dụng render miễn phí đang bị giới hạn, bạn có thể truy cập vào lúc khác nhé. Cảm ơn bạn đã ghé thăm!"
-  );
   return (
     <>
       <ChatClient />
